9781a85455dec992382bdc10ae7d4bcf6e3ce7b3
dhayanandhs2003 <sakthideepa77@gmail.com>
2025-04-08 20:36:46 +0530

    constructor overloading or multiple constructors

---
+++ src/constructors/MultipleConstructors.java
@@ -0,0 +1,39 @@
+// constructor overloading
+package constructors;
+
+class Book {
+	int pages;
+	String book_name;
+	
+	// @override
+	Book(){
+		pages = 25;
+		book_name = "Maari";
+	}
+	
+	Book(int p, String bn){
+		pages = p;
+		book_name = bn;
+	}
+	
+	public void show() {
+		System.out.println(pages + " " + book_name);
+	}
+	
+}
+
+public class MultipleConstructors {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Book book1 = new Book(9,"men");
+		book1.show();
+		// book.Book(8,"Welcome"); -- this wont work
+		
+		Book book2 = new Book();
+		book2.show();
+
+	}
+
+}
